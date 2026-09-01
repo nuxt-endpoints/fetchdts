b6ad03f3120a4a811c34ee15f7203a3bfb6dadc0
Yoshinori Ishii <y.ishii@azoom.jp>
2026-09-01 15:23:15 +0900

    build: make source exports opt-in

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['src/index.ts', 'src/compiler.ts'],
   dts: { oxc: true },
-  exports: { devExports: true },
+  exports: { devExports: 'fetchdts-src' },
 })
